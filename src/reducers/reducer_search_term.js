6845bbe6e96d5b5668c579bb1992dcf8d7ec4a57
Son Trinh <xuanson.trinh@outlook.com>
2017-11-22 17:38:23 +0700

    updated

--- src/reducers/reducer_search_term.js
+++ src/reducers/reducer_search_term.js
@@ -1,9 +1,8 @@
 export default function(state = null, action) {
   switch (action.type) {
     case 'CHANGE_TERM':
-    return action.payload;
-    break;
+      return action.payload;
   }
 
-  return '';
+  return state;
 }
